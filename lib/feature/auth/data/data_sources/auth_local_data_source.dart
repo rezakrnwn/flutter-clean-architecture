ead741b7743cdd07daa87d09a5f48c3afb00c176
Reza Kurniawan <kurniawanreza94@gmail.com>
2022-05-05 13:45:37 +0700

    add variable class name for instance name service locator

--- lib/feature/auth/data/data_sources/auth_local_data_source.dart
+++ lib/feature/auth/data/data_sources/auth_local_data_source.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_clean_architecture/feature/auth/data/models/user_model.dart';
 
 abstract class AuthLocalDataSource {
+  static const className = "AuthLocalDataSource";
+
   UserModel getUser(String username);
 }
--- lib/feature/auth/domain/repositories/auth_repository.dart
+++ lib/feature/auth/domain/repositories/auth_repository.dart
@@ -2,5 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_clean_architecture/feature/auth/domain/entities/user.dart';
 
 abstract class AuthRepository {
+  static const className = "AuthRepository";
+
   Either<Exception, User> getUser(String username);
 }

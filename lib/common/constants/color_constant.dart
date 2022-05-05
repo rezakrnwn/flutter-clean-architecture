0cdef767c1d480911346b3fc98062d19171f98c7
Reza Kurniawan <kurniawanreza94@gmail.com>
2022-05-05 13:54:11 +0700

    add color constant

---
+++ lib/common/constants/color_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static const Color lightPrimary = Color(0xFF00AA13);
+  static const Color darkPrimary = Color(0xFF16161C);
+  static const Color lightAccent = Color(0xFF00AA13);
+  static const Color darkAccent = Color(0xFF069516);
+  static const Color lightBG = Color(0xFFFFFFFF);
+  static const Color darkBG = Color(0xFF212121);
+  static const Color grey = Color(0xFFFBFBFB);
+  static const Color lightPlaceHolder = Color(0xFFC2C2C2);
+  static const Color darkPlaceHolder = Color(0xFFBEBEBE);
+}
